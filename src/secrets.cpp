0f45a78193da3178b4d47172d06d8271fc41fffc
Gaston Segura <gastonsegura2908@mi.unc.edu.ar>
2025-05-05 09:37:18 -0300

    remove files

--- src/secrets.cpp
+++
@@ -1,6 +0,0 @@
-#include "secrets.h"
-
-const char* ssid = "MERCUSYS_4C8C";
-const char* password = "inaramelu";
-const char* api_url = "http://192.168.0.103:5000/temperatura";
-const char* api_key = "SecretAPIKey";
